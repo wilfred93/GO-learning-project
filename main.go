ddc4f2fce798063b0ac68662186882560227af22
Alfred <wilfred.lazaro@gmail.com>
2022-11-09 06:57:48 +0100

    start GO learning project

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v bookin application\n", conferenceName)
+	fmt.Println("We have a total of", remainingTickets , "remaining tickets")
+	fmt.Println("Get your tickets here to attend")
+
+	
+
+}
